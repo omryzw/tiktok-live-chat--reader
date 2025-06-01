b797b9f11872c25fe25194c4b6c4c6886fe00173
Omri <mareberao@gmail.com>
2025-06-01 10:40:01 +0200

    Add user input for TikTok username and refactor connection setup

--- index.mjs
+++ index.mjs
@@ -1,30 +1,39 @@
 // Username of someone who is currently live
 import { TikTokLiveConnection, WebcastEvent } from 'tiktok-live-connector';
 import say from 'say';
+import readline from 'readline';
 
-const tiktokUsername = 'lilkudzivert';
-
-// Create a new wrapper object and pass the username
-const connection = new TikTokLiveConnection(tiktokUsername);
-
-// Connect to the chat (await can be used as well)
-connection.connect().then(state => {
-    console.info(`Connected to roomId ${state.roomId}`);
-}).catch(err => {
-    console.error('Failed to connect', err);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-// Define the events that you want to handle
-// In this case we listen to chat messages (comments)
-connection.on(WebcastEvent.CHAT, data => {
-    const message = `${data.user.uniqueId} (userId:${data.user.uniqueId}) writes: ${data.comment}`;
-    console.log(message);
-    say.speak(data.comment); // Use say to read the comment aloud
-});
+rl.question('Enter TikTok username: ', (inputUsername) => {
+    const tiktokUsername = inputUsername || 'lilkudzivert';
+    // Create a new wrapper object and pass the username
+    const connection = new TikTokLiveConnection(tiktokUsername);
 
-// And here we receive gifts sent to the streamer
-connection.on(WebcastEvent.GIFT, data => {
-    console.log(`${data.user.uniqueId} (userId:${data.user.userId}) sends ${data.giftId}`);
-});
+    // Connect to the chat (await can be used as well)
+    connection.connect().then(state => {
+        console.info(`Connected to roomId ${state.roomId}`);
+    }).catch(err => {
+        console.error('Failed to connect', err);
+    });
 
-// ...and more events described in the documentation below
+    // Define the events that you want to handle
+    // In this case we listen to chat messages (comments)
+    connection.on(WebcastEvent.CHAT, data => {
+        const message = `${data.user.uniqueId} (userId:${data.user.uniqueId}) writes: ${data.comment}`;
+        console.log(message);
+        say.speak(data.comment); // Use say to read the comment aloud
+    });
+
+    // And here we receive gifts sent to the streamer
+    connection.on(WebcastEvent.GIFT, data => {
+        console.log(`${data.user.uniqueId} (userId:${data.user.userId}) sends ${data.giftId}`);
+    });
+
+    // ...and more events described in the documentation below
+
+    rl.close();
+});
